209e8e418c3e0b2c724a9d26e1f9a3b268c11361
Peter Babič <peter@peterbabic.dev>
2021-01-10 09:57:15 +0100

    add level switcher

---
+++ src/levels.js
@@ -0,0 +1,13 @@
+export const levels = [
+  { side: 3, variant: 0, holes: [0, 3] }, // 2 / 2 (100%)
+  { side: 4, variant: 0, holes: [0, 7] }, // 168 / 39 (23.21%%)
+  { side: 4, variant: 1, holes: [0, 1] }, // 88 / 14 (15.91%)
+  { side: 4, variant: 2, holes: [0, 3] }, // 22 / 3 (13.64%)
+  { side: 5, variant: 0, holes: [0, 10] }, // 771424 / 81668 (10.59%)
+  { side: 5, variant: 1, holes: [0, 11] }, // 319058 / 22368 (7.01%)
+  { side: 5, variant: 2, holes: [0, 7] }, // 157024 / 7339 (4.67%)
+  { side: 5, variant: 3, holes: [3] }, // 1149568 / 85258 (7.42%)
+  { side: 5, variant: 4, holes: [0] }, // 568630 / 29760 (5.23%)
+  { side: 5, variant: 5, holes: [1] }, // 294543 / 14880 (5.05%)
+  { side: 5, variant: 6, holes: [4] }, // 137864 / 1550 (1.12%)
+]
