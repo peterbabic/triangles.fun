f9e8fadddcb1ae53fe66412b447342a1dc5a7d79
Peter Babič <peter@peterbabic.dev>
2021-03-08 19:32:04 +0100

    remove global css

--- src/App.svelte
+++ src/App.svelte
@@ -14,40 +14,28 @@
 </script>
 
 <style>
-  .level {
-    cursor: pointer;
-    margin-right: 10px;
-    color: hsl(219, 28%, 88%);
-  }
-
   .active {
-    font-weight: bold;
-    color: hsl(220, 17%, 32%);
+    @apply font-bold text-gray-800;
   }
 
   .completed {
-    background-color: hsl(220, 16%, 36%);
-    color: hsl(218, 27%, 94%);
-  }
-
-  :global(body) {
-    background-color: #eceff4;
-    margin: 0;
-    padding: 15px;
+    @apply text-gray-800 bg-gray-200;
   }
 </style>
 
 <Tailwind />
 
-<main>
-  <Triangle {side} {variant} />
+<main class="container mx-auto text-center">
+  <div class="level">
+    {#each levels as _, i}
+      <button
+        class="cursor-pointer mr-5"
+        on:click={changeLevel}
+        value={i}
+        class:completed={$completed.includes(i)}
+        class:active={level == i}>{i + 1}</button>
+    {/each}
+  </div>
 
-  {#each levels as _, i}
-    <button
-      class="level"
-      on:click={changeLevel}
-      value={i}
-      class:completed={$completed.includes(i)}
-      class:active={level == i}>{i + 1}</button>
-  {/each}
+  <Triangle {side} {variant} />
 </main>
